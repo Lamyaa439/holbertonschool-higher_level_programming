47a87593054d199778f393108f9ffe797ca35a45
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-11-25 06:58:56 +0000

    Update 0-positive_or_negative.py file

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,7 +3,7 @@ import random as ra
 number = ra.randint(-100, 100)
 if number < 0:
     print(f"{number} is negative")
-elif number > 0:
+if number > 0:
     print(f"{number} is positive")
-elif number == 0:
+if number == 0:
     print(f"{number} is zero")
