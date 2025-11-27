aa59390b8a2f46b81b75acb4e3aed6db28e3486b
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-11-27 08:57:41 +0000

    Update 100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,7 +4,7 @@ value = 0
 while 65 <= i <= 90:
     if i % 2 == 0:
         value = i + 32
-        print(chr(value), end="")
     else:
-        print(chr(i), end="")
+        value = i
+    print("{}".format(chr(value)), end="")
     i -= 1
