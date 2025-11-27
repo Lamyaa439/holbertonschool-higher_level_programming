b8817e2eb6033c13766448c20d0f7fb2533a7ed6
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-11-27 07:02:39 +0000

    Update 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for i in range(0, 10):
     for j in range(1, 10):
         if i == 8 and j == 9:
             print("{}{}\n".format(i, j), end="")
-        elif(i < j):
+        elif (i < j):
             print("{}{}, ".format(i, j), end="")
