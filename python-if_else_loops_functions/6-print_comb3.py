c3d0e888033afe439d09b14d865f1016c7d610c0
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-11-27 07:00:37 +0000

    Update 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(1, 10):
+        if i == 8 and j == 9:
+            print("{}{}\n".format(i, j), end="")
+        elif(i < j):
+            print("{}{}, ".format(i, j), end="")
