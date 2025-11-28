e0c2fd8a73e6d2b5d7588d1c33db53b002352e3e
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-11-28 16:55:18 +0000

    Update 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,6 +3,6 @@ import sys
 if __name__ == "__main__":
     arg_list = sys.argv
     len_list = len(arg_list)
-    print("{} argument:".format(len_list - 1))
+    print("{} arguments:".format(len_list - 1))
     for i in range(1, len_list):
         print("{}: {}".format(i, arg_list[i]))
