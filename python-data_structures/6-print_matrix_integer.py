b54c37bb3662dc6556f303967d8c3a785edce9f1
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-11-30 08:42:19 +0000

    Update 6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print(*row)
+        for i in range(len(row)):
+            if i == len(row) - 1:
+                separator = ""
+            else:
+                separator = " "
+            print("{}".format(row[i]), end = separator)
+        print()
