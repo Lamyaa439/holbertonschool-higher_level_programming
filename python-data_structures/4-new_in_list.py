61a8089283cb34e6b6fed1b665a5c8d927bc0319
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-11-30 07:19:49 +0000

    Update 4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,8 +3,8 @@ def new_in_list(my_list, idx, element):
     new_list = []
     for i in my_list:
         new_list.append(i)
-    len_newlist = len(new_list) - 1
-    for j in range(0, len_newlist):
+    len_newlist = len(new_list) 
+    for j in range(len_newlist):
         if j == idx:
             new_list[j] = element
     return new_list
