50d6b4bf0e881e2e1913ee851956d56f42cc44fa
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-11-30 06:28:26 +0000

    Update  0-main.py

--- python-data_structures/0-main.py
+++ python-data_structures/0-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from 0-print_list_integer import print_list_integer
+print_list_integer = __import__('0-print_list_integer').print_list_integer
 
 if __name__ == "__name__":
     my_list = [1, 2, 3, 4, 5]
