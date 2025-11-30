4fd723df7dfdafcc1d6e71ec9e2c997657733546
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-11-30 08:32:05 +0000

    Update 6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for cloum in row:
-            print("{:d}".format(cloum), end=" ")
-        print()
-
+        print(*row)
