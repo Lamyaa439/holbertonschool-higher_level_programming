60a174aa1eef43a9b25423ac8372155132c67843
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-12-02 21:19:02 +0000

    Update 11-delete_at.py

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    del my_list[idx]
+    if idx < len(my_list):
+        del my_list[idx]
+    else:
+        del my_list[idx - 1]
     return my_list
