102d7ea47220bbc287bc840a18c1464d3d9c14fa
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-12-02 09:59:47 +0000

    Update 10-divisible_by_2.py

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = []
+    to_list = 0
+    for i in my_list:
+        if i % 2 == 0:
+            to_list = True
+        else:
+            to_list = False
+        new_list.append(to_list)
+    return new_list 
