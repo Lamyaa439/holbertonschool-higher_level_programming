7dff05687d037c0c61380d82f42f159ec2d2684f
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-12-03 13:37:03 +0000

    Update 0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    square = 0
+    for row in matrix:
+        for coulm in row:
+            square = matrix[row][coulm]
+            square **= 2
+            new_matrix[row][matrix] += square
+    return new_matrix
