0db16102e1c9abf1af4db499a984a9cd836d8f53
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-12-03 14:06:22 +0000

    Update 1-search_replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    """
+my_list: is the initial list
+search: is the element to replace in the list
+replace: is the new element
+    """
+    new_list = []
+    for i in my_list:
+        if my_list[i] == search:
+            new_list[i] = replace
+        else:
+            new_list[i] = my_list[i]
+    return new_list
 
