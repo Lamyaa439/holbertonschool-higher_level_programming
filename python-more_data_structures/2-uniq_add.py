ba048e4779fc4615995f111ed105502b37d88f0e
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-12-03 14:28:28 +0000

    Update 2-uniq_add.py

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    sum_ = 0
+    new_list = []
+    for i in my_list:
+        if i in new_list:
+            continue
+        else:
+            new_list.append(i)
+    for j in new_list:
+        sum_ += j
+    return sum_
