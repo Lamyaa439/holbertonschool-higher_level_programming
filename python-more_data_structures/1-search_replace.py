64a3bd9fdb9cbf9d31f06cbbf2be939431c2798d
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-12-03 14:11:19 +0000

    Update 1-search_replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -6,10 +6,10 @@ search: is the element to replace in the list
 replace: is the new element
     """
     new_list = []
-    for i in my_list:
+    for i in range(len(my_list)):
         if my_list[i] == search:
-            new_list[i] = replace
+            new_list.append(replace)
         else:
-            new_list[i] = my_list[i]
+            new_list.append(my_list[i])
     return new_list
 
