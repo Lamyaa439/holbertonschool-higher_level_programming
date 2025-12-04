0c867ae886ab001763927885ac1ff85d42798334
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-12-04 07:57:46 +0000

    Update 4-only_diff_elements.py

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
+    common = []
     diff_element = []
     for i in set_1:
         for j in set_2:
             if i == j:
-                continue
-            else:
-                diff_element.append(j)
-                diff_element.append(i)
+                common.append(j)
+    for i in set_1:
+        if i in common:
+            continue
+        else:
+            diff_element.append(i)
+    for j in set_2:
+        if j in common:
+            continue
+        else:
+            diff_element.append(j)
     return diff_element
