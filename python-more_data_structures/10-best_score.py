e464f1ae2d7766cddd0b8021e1ecd0c70e78707d
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-12-05 10:48:07 +0000

    Update 10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
     max_value = 0
+    max_key = ""
     for key, value in a_dictionary.items():
         if max_value < value:
-            return key
+            max_value = value
+            max_key = key
+    return max_key
