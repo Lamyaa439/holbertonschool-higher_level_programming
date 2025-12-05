0f6245ae871ce89bd9ed6528e1b09c0339bd8f4b
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-12-05 09:32:36 +0000

    Update 6-print_sorted_dictionary.py

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    list_item = []
+    for item in a_dictionary.items():
+        list_item.append(item)
+    list_item.sort()
+    for key, value in list_item:
+        print("{}: {}".format(key, value))
