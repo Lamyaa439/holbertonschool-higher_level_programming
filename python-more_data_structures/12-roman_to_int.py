55081f380bfe87f428878c8c9ed772e777a1e8fe
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-12-06 08:42:17 +0000

    Upadte 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,36 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500}
-    int_ = 0
-    to_int = 0
-    for i in roman_string:
-        int_ = roman_dict[i]
-        to_int += int_
-    return to_int
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman_dict = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
+    result = 0
+    length = len(roman_string)
+
+    for i in range(length):
+        # Get the value of the current character
+        current_value = roman_dict.get(roman_string[i], 0)
+        
+        # Check if there is a next character
+        if i < length - 1:
+            next_value = roman_dict.get(roman_string[i + 1], 0)
+            
+            # Subtraction Rule (e.g., IV: 1 < 5)
+            if current_value < next_value:
+                result -= current_value
+            else:
+                result += current_value
+        else:
+            # Always add the last character
+            result += current_value
+
+    return result
