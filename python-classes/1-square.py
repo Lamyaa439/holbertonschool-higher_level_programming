68ee88f76eee73ecc7c28096f869ece427a0398c
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-12-08 19:55:20 +0000

    Create 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+0-square.py moduls to create a class
+"""
+
+
+class Square:
+    """
+    class Square that defines a square
+    """
+    def __init__(self, size):
+        """
+        method to define Private instance attribute
+        """
+        self.__size = size
