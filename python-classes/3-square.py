b107a2d89017371cc067ed5fc9c3bf8517375580
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-12-08 20:46:36 +0000

    Update 3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+0-square.py moduls to create a class
+"""
+
+
+class Square:
+    """
+    class Square that defines a square
+    """
+    def __init__(self, size=0):
+        """
+        method to define Private instance attribute
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        """
+        method to returns the current square area
+        """
+        return self.size ** 2
+
