1a6c2a133315f5ef5f435c2e3c2d8367e6f11ab0
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-12-09 06:33:48 +0000

    Update 6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -67,7 +67,8 @@ class Square:
         if self.__size == 0:
             print()
             return
-        
+        for i in range(self.__position[1]):
+            print("")
         for i in range(self.__size):
             spaces = " " * self.position[0]
             print("{}".format(spaces), end="")
