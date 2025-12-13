4d976a94ffdaee185290687618b6da9f65d92711
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-12-13 16:54:26 +0000

    Update 8-rectangle.py

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -74,7 +74,7 @@ class Rectangle:
             sum1 = self.__width + self.__height
             perimeter = 2 * sum1
         return perimeter
-    
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """
@@ -82,10 +82,8 @@ class Rectangle:
         """
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-
         if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        
+            raise TypeError("rect_2 must be an instance of Rectangle")        
         if rect_1.area() >= rect_2.area():
             return rect_1
         else:
