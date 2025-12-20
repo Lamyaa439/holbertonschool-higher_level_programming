c9b053835f28bd199953821b4814b43c258ad9f0
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-12-20 14:53:49 +0000

    Update 7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -20,6 +20,5 @@ class BaseGeometry:
         """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
-
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
