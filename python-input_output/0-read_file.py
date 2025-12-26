ff11eb0673d5d24d33fe2f3ca98a1019c7068436
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-12-26 22:42:04 +0000

    Update 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+0-read_file.py moduls to read file
+"""
+
+
 def read_file(filename=""):
+    """
+    This function to read file 
+    """
     with open(filename) as f:
         f_contents = f.read()
         print(f_contents, end="")
