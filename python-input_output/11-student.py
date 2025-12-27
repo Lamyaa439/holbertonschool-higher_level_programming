6cdb93d998324c14e3c4c32e47343fcd30cd9cdb
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-12-27 19:21:38 +0000

    Update 11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -5,7 +5,10 @@
 
 
 class Student:
-    """Defines a student with serialization and deserialization capabilities."""
+    """
+    Defines a student with serialization
+    and deserialization capabilities.
+    """
 
     def __init__(self, first_name, last_name, age):
         """Initializes the student instance."""
@@ -14,7 +17,9 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of a Student instance."""
+        """
+        Retrieves a dictionary representation of a Student instance.
+        """
         if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
             res = {}
             for k in attrs:
@@ -24,7 +29,10 @@ class Student:
         return self.__dict__
 
     def reload_from_json(self, json):
-        """Replaces all attributes of the Student instance from a dictionary."""
+        """
+        Replaces all attributes of the Student
+        instance from a dictionary.
+        """
         for key, value in json.items():
             if key in self.__dict__:
                 self.__dict__[key] = value
