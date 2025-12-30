8b0f12004349b5eabf3375273cd50b53f7d3061a
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-12-30 14:35:29 +0000

    Update task_00_basic_serialization.py

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -12,7 +12,7 @@ def serialize_and_save_to_file(data, filename):
     filename: The filename of the output JSON file.
     If the output file already exists it should be replaced.
     """
-    with open(filename, "wb") as f:
+    with open(filename, "w") as f:
         json.dump(data, f)
 
 def load_and_deserialize(filename):
@@ -21,6 +21,5 @@ def load_and_deserialize(filename):
     filename: The filename of the input JSON file This function returns
     a Python Dictionary with the deseialized JSON data from the file.
     """
-    with open(filename, "rb") as f:
-        return pickle.load(f)
-
+    with open(filename, "r") as f:
+        return json.load(f)
