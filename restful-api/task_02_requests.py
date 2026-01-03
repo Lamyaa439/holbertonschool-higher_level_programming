242fb5f43f2fb554eb9169638441b1ff75553a76
Lamyaa439 <lamyaaalghaihab@gmail.com>
2026-01-03 14:06:52 +0000

    Update task_02_requests.py

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import requests
+import csv
+
+def fetch_and_print_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    r = requests.get(url)
+
+    print(f"Status Code: {r.status_code}")
+
+    if r.status_code == 200:
+        posts = r.json()
+        for post in posts:
+            print(post.get("title"))
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    r = requests.get(url)
+
+    if r.status_code == 200:
+        posts = r.json()
+
+    data_toSave = []
+    for post in posts:
+        data_toSave.append({
+            "id": post("id"),
+            "title": post("title"),
+            "body": post("body")
+            })
+
+    with open("posts.csv", mode='w', encoding='utf-8', newline='') as f:
+        fieldnames = ['id', 'title', 'body']
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer.writerows(filtered_data)
