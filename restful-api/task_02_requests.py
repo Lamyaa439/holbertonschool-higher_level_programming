b43506002d813e0bbe8662146b5487a2e9a758f9
Lamyaa439 <lamyaaalghaihab@gmail.com>
2026-01-03 14:14:55 +0000

    Update task_02_requests.py

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -20,17 +20,20 @@ def fetch_and_save_posts():
     if r.status_code == 200:
         posts = r.json()
 
-    data_toSave = []
-    for post in posts:
-        data_toSave.append({
+    data_toSave = [
+            {
             "id": post["id"],
             "title": post["title"],
             "body": post["body"]
-            })
+            }
+            for post in posts
+            ]
 
     with open("posts.csv", mode='w', encoding='utf-8', newline='') as f:
         fieldnames = ['id', 'title', 'body']
         writer = csv.DictWriter(f, fieldnames=fieldnames)
-
         writer.writeheader()
         writer.writerows(data_toSave)
+
+    else:
+        pass
