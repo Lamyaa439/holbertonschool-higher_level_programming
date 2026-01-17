79a08873fb6c59bfd0b15bcbc8a2a459c52116c1
Lamyaa439 <lamyaaalghaihab@gmail.com>
2026-01-17 23:55:16 +0300

    Create

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Prints the State object with the name passed as argument from the database
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def main():
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            user, passwd, db_name
+        ),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == state_name).first()
+
+    if state is None:
+        print("Not found")
+    else:
+        print("{}".format(state.id))
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
