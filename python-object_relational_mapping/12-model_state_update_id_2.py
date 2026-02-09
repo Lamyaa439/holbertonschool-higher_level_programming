43063159675e8e2e8ea74cb87084d4f07e33582a
Lamyaa439 <lamyaaalghaihab@gmail.com>
2026-02-09 12:28:24 +0300

    Update 12-model_state_update_id_2.py

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,38 +1,28 @@
 #!/usr/bin/python3
 """
-Script that changes the name of a State object from the database hbtn_0e_6_usa.
-Changes the name of the State where id = 2 to New Mexico.
+Script that changes the name of a State object from the database hbtn_0e_6_usa
+Changes the name of the State where id = 2 to New Mexico
 """
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 
 
 if __name__ == "__main__":
-    # Get command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Create engine connection to MySQL server
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                          .format(username, password, database),
-                          pool_pre_ping=True)
-
+    # Create engine to connect to MySQL server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    
     # Create a configured "Session" class
-    Session = sessionmaker(bind=engine)
-
-    # Create a Session instance
-    session = Session()
-
-    # Query the State with id = 2
+    session = Session(engine)
+    
+    # Query the state with id = 2 and update its name
     state_to_update = session.query(State).filter(State.id == 2).first()
-
-    # Update the name if the state exists
+    
     if state_to_update:
         state_to_update.name = "New Mexico"
         session.commit()
-
+    
     # Close the session
     session.close()
